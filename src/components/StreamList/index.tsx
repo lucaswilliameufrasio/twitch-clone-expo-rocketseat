fa87e910c463c557eae5cca523177d1d5b215e3b
Lucas William <lucaswilliameufrasio@gmail.com>
2020-11-14 05:46:37 -0300

    feat: add Following view components

---
+++ src/components/StreamList/index.tsx
@@ -0,0 +1,64 @@
+import React from "react";
+
+import streamThumbnail from "../../images/stream_thumbnail.jpg";
+
+import {
+  List,
+  StreamContainer,
+  StreamRow,
+  StreamThumbnail,
+  StreamColumn,
+  StreamHeader,
+  StreamAvatar,
+  StreamUsername,
+  StreamDescription,
+  StreamCategory,
+  TagRow,
+  TagView,
+  TagText,
+} from "./styles";
+
+const StreamList: React.FC = () => {
+  const StreamItem: React.FC = () => (
+    <StreamContainer>
+      <StreamThumbnail source={streamThumbnail} />
+
+      <StreamColumn>
+        <StreamRow>
+          <StreamHeader>
+            <StreamAvatar />
+            <StreamUsername numberOfLines={1}>
+              lucaswilliameufrasio
+            </StreamUsername>
+          </StreamHeader>
+
+          <StreamDescription numberOfLines={1}>Backend com Node.JS, Typescript, Clean Architecture</StreamDescription>
+          
+          <StreamCategory numberOfLines={1}>
+            Science {"&"} Technology
+          </StreamCategory>
+        </StreamRow>
+
+        <TagRow>
+          <TagView>
+            <TagText>Portuguese</TagText>
+          </TagView>
+          <TagView>
+            <TagText>Backend Development</TagText>
+          </TagView>
+        </TagRow>
+      </StreamColumn>
+    </StreamContainer>
+  );
+
+  return (
+    <List>
+      <StreamItem />
+      <StreamItem />
+      <StreamItem />
+      <StreamItem />
+    </List>
+  );
+};
+
+export default StreamList;
